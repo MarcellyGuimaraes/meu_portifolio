486afd32f67f2078e21ff003af6c32cd33d975fa
MarcellyGuimaraes <souzaguimaraesmarcelly@gmail.com>
2023-02-09 17:57:10 -0300

    feat: criando navbar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './header.css'
 
 const Header = () => {
-  return <div>Header</div>
+  const [toggle, setToggle] = useState(false)
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="." className="nav__logo">
+          Marcelly!
+        </a>
+        <div className={toggle ? 'nav__menu show-menu' : 'nav__menu'}>
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="#home" className="nav__link active-link">
+                <i className="uil uil-estate nav__icon"></i> Início
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="uil uil-user nav__icon"></i> Sobre
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                <i className="uil uil-file-alt nav__icon"></i> Skills
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#services" className="nav__link">
+                <i className="uil uil-briefcase-alt nav__icon"></i> Serviços
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#portifolio" className="nav__link">
+                <i className="uil uil-scenery nav__icon"></i> Portifólio
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#contato" className="nav__link">
+                <i className="uil uil-message nav__icon"></i> Contato
+              </a>
+            </li>
+          </ul>
+
+          <i
+            className="uil uil-times nav__close"
+            onClick={() => setToggle(!toggle)}
+          ></i>
+        </div>
+
+        <div className="nav__toggle" onClick={() => setToggle(!toggle)}>
+          <i className="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
