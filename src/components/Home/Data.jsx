d7ef3b37b8606410c1fd79b8e31e8f9d2e96d03e
MarcellyGuimaraes <souzaguimaraesmarcelly@gmail.com>
2023-02-09 18:44:07 -0300

    feat: criando parte 'Home'

---
+++ src/components/Home/Data.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Data = () => {
+  return (
+    <div className="home__data">
+      <h1 className="home__title">Marcelly Guimarães</h1>
+      <h3 className="home__subtitle">Desenvolvedora Frontend</h3>
+      <p className="home__description">
+        Eu sou desenvolvedora há 3 anos com foco em frontend e estou aprendendo
+        também backend.
+      </p>
+
+      <a href="#contato" className="button button--flex">
+        Fale Comigo!
+      </a>
+    </div>
+  )
+}
+
+export default Data
---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="" target="_blank" className="home__social-icon">
+        <i className="uil uil-github-alt"></i>
+      </a>
+      <a href="" target="_blank" className="home__social-icon">
+        <i className="uil uil-linkedin-alt"></i>
+      </a>
+      <a href="" target="_blank" className="home__social-icon">
+        <i className="uil uil-envelope-alt"></i>
+      </a>
+    </div>
+  )
+}
+
+export default Social
