f0677f71957761ab19a77d85192c397ec11c7e05
MarcellyGuimaraes <souzaguimaraesmarcelly@gmail.com>
2023-02-10 19:15:10 -0300

    fix: Mudando texto de introdução e adicionando links no 'Home'

--- src/components/Home/Data.jsx
+++ src/components/Home/Data.jsx
@@ -6,8 +6,9 @@ const Data = () => {
       <h1 className="home__title">Marcelly Guimarães</h1>
       <h3 className="home__subtitle">Desenvolvedora Frontend</h3>
       <p className="home__description">
-        Eu sou desenvolvedora há 3 anos com foco em frontend e estou aprendendo
-        também backend.
+        Desenvolvedora web frontend com 3 anos de experiência em JavaScript,
+        CSS, HTML, React, Tailwind e Next. Focada em design e experiência do
+        usuário.
       </p>
 
       <a href="#contato" className="button button--flex">
--- src/components/Home/Social.jsx
+++ src/components/Home/Social.jsx
@@ -3,13 +3,25 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-      <a href="" target="_blank" className="home__social-icon">
+      <a
+        href="https://github.com/MarcellyGuimaraes"
+        target="_blank"
+        className="home__social-icon"
+      >
         <i className="uil uil-github-alt"></i>
       </a>
-      <a href="" target="_blank" className="home__social-icon">
+      <a
+        href="https://www.linkedin.com/in/marcellyguimaraes/"
+        target="_blank"
+        className="home__social-icon"
+      >
         <i className="uil uil-linkedin-alt"></i>
       </a>
-      <a href="" target="_blank" className="home__social-icon">
+      <a
+        href="mailto:souzaguimaraesmarcelly@gmail.com"
+        target="_blank"
+        className="home__social-icon"
+      >
         <i className="uil uil-envelope-alt"></i>
       </a>
     </div>
